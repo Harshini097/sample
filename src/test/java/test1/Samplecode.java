c034f5241e45d61535b9fbede6411852131b8a0e
Harshini097 <harshiniganesan97@gmail.com>
2022-12-22 12:29:21 +0530

    code

--- src/test/java/test1/Samplecode.java
+++ src/test/java/test1/Samplecode.java
@@ -3,8 +3,8 @@ package test1;
 public class Samplecode {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
-		System.out.println(".....Welcome Harshini....");
+		System.out.println("Hello Hi...");
+		System.out.println("....Welcome Harshini....");
 
 	}
 
