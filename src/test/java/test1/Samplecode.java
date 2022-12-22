9665778b9fadf4e761234eb2d7bc9ac106f415dd
Harshini097 <harshiniganesan97@gmail.com>
2022-12-22 15:26:38 +0530

    saleem's code

--- src/test/java/test1/Samplecode.java
+++ src/test/java/test1/Samplecode.java
@@ -5,6 +5,8 @@ public class Samplecode {
 	public static void main(String[] args) {
 		System.out.println("Hello Hi...");
 		System.out.println("....Welcome Harshini....");
+		int a1 = 10;
+		System.out.println("value of a1 is :" +a1);
 
 	}
 
